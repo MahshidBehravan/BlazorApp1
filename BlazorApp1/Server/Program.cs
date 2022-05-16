75d5953d9654c5a8a2dd7f24713460b1f2994522
Mahshid <mahshid.behravan@hotmail.com>
2022-05-16 09:58:46 -0400

    Add project files.

---
+++ BlazorApp1/Server/Program.cs
@@ -0,0 +1,48 @@
+global using BlazorApp1.Shared;
+global using Microsoft.EntityFrameworkCore;
+global using BlazorApp1.Server.Data;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+builder.Services.AddEndpointsApiExplorer();// I Add T=it for swagger
+builder.Services.AddSwaggerGen();// swager
+
+var app = builder.Build();
+
+app.UseSwaggerUI();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseSwagger();
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
